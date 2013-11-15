c3dc296e5d6a1e1d0971b586b38e189e9f12e5d4
sea-kg <mrseakg@gamail.com>
2013-11-15 23:52:29 +0700

    added create_clip

---
+++ create_clip/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+qmake create_clip.pro
+
+make
+
+./create_clip
+if [ -f video.avi ]; then
+	rm video.avi
+fi
+	
+	
+# avconv -f image2 -r 30 -i images/clip-%06d.png video.avi
+
+avconv -f image2 -r 30 -i images/clip-%06d.png -i source.mp3 -acodec copy -b 64k video.avi
